11fb7454a04bd63b1d85cf6332e6250727eb7d6f
Alan <weiJX@mimoprint.com>
2020-06-10 11:11:46 +0800

    对接新浪接口

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,112 +1,117 @@
 <template>
-  <div id="Home">
-    <input type="text" v-model="stockCode" />
+    <div id="Home">
+        <input type="text" v-model="stockCode" />
 
-    <div class="btn-group">
-      <div class="btn btn-start" @tap="startListen">开始侦听</div>
-      <div class="btn btn-end" @tap="stopListen">停止监听</div>
+        <div class="btn-group">
+            <div class="btn btn-start" @click="startListen">开始侦听</div>
+            <div class="btn btn-end" @click="stopListen">停止监听</div>
+        </div>
+
+        <div class="stockBoard">
+            <div class="stockInfo">
+                <div class="stockName">xxxx</div>
+                <div class="stockNum">12345</div>
+            </div>
+
+            <div class="price-data-wrap">
+                <ul class="price-data-list">
+                    <li class="price-data-item">
+                        <div class="price-data-label">当前价格：</div>
+                        <div class="price-data-content">12.22</div>
+                    </li>
+                    <li class="price-data-item">
+                        <div class="price-data-label">今日开盘价：</div>
+                        <div class="price-data-content">50.22</div>
+                    </li>
+                    <li class="price-data-item">
+                        <div class="price-data-label">昨日收盘价：</div>
+                        <div class="price-data-content">50.22</div>
+                    </li>
+                    <li class="price-data-item">
+                        <div class="price-data-label">今日最高价</div>
+                        <div class="price-data-content">50.22</div>
+                    </li>
+                    <li class="price-data-item">
+                        <div class="price-data-label">今日最低价</div>
+                        <div class="price-data-content">50.22</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
-
-    <div class="stockBoard">
-      <div class="stockInfo">
-        <div class="stockName">xxxx</div>
-        <div class="stockNum">12345</div>
-      </div>
-
-      <div class="price-data-wrap">
-        <ul class="price-data-list">
-          <li class="price-data-item">
-            <div class="price-data-label">当前价格：</div>
-            <div class="price-data-content">12.22</div>
-          </li>
-          <li class="price-data-item">
-            <div class="price-data-label">今日开盘价：</div>
-            <div class="price-data-content">50.22</div>
-          </li>
-          <li class="price-data-item">
-            <div class="price-data-label">昨日收盘价：</div>
-            <div class="price-data-content">50.22</div>
-          </li>
-          <li class="price-data-item">
-            <div class="price-data-label">今日最高价</div>
-            <div class="price-data-content">50.22</div>
-          </li>
-          <li class="price-data-item">
-            <div class="price-data-label">今日最低价</div>
-            <div class="price-data-content">50.22</div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
-  data() {
-    return {
-      stockCode: ""
-    };
-  },
-  methods: {
-    startListen() {},
+    name: "Home",
+    data() {
+        return {
+            stockCode: "",
+            interval: null
+        };
+    },
+    methods: {
+        startListen() {
+            if (this.interval) return;
+            this.interval = setInterval(() => {
+                this.$store.dispatch("stock/getStocks");
+            }, 1000);
+        },
 
-    stopListen() {}
-  },
-  mounted() {
-    this.$store.dispatch('stock/getStocks');
-  },
+        stopListen() {
+            clearInterval(this.interval);
+        }
+    },
+    mounted() {}
 };
 </script>
 
 <style lang="scss">
 #Home {
-  .btn-group {
-    display: flex;
-    justify-content: center;
-
-    .btn {
-      background-color: pink;
-      width: 1.8rem;
-      height: .8rem;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin: .1rem;
-      color: #ffffff;
-      font-size: .28rem;
-    }
-  }
-
-  .stockBoard{
-    font-size: .28rem;
-    padding: .3rem;
-    .stockInfo{
-      display: flex;
-      font-weight: bold;
-
-      .stockName{
-        margin-right: .1rem;
-      }
-
-      .stockNum{
-
-      }
-    }
-  }
-
-  .price-data-wrap{
-    border: 1px solid black;
-    margin-top: .3rem;
-    padding: .1rem;
-
-    .price-data-list{
-      .price-data-item{
-        margin-top: .1rem;
+    .btn-group {
         display: flex;
-      }
+        justify-content: center;
+
+        .btn {
+            background-color: pink;
+            width: 1.8rem;
+            height: 0.8rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0.1rem;
+            color: #ffffff;
+            font-size: 0.28rem;
+        }
     }
-  }
+
+    .stockBoard {
+        font-size: 0.28rem;
+        padding: 0.3rem;
+        .stockInfo {
+            display: flex;
+            font-weight: bold;
+
+            .stockName {
+                margin-right: 0.1rem;
+            }
+
+            .stockNum {
+            }
+        }
+    }
+
+    .price-data-wrap {
+        border: 1px solid black;
+        margin-top: 0.3rem;
+        padding: 0.1rem;
+
+        .price-data-list {
+            .price-data-item {
+                margin-top: 0.1rem;
+                display: flex;
+            }
+        }
+    }
 }
 </style>
--- src/store/modules/stock.js
+++ src/store/modules/stock.js
@@ -1,35 +1,85 @@
 import * as Api from "@/common/api.js";
 
+const formater = [
+    { i: -1, key: "stockCode", alias_zh: "股票代号" },
+    { i: 0, key: "stockName", alias_zh: "股票名称" },
+    { i: 1, key: "openingPrice", alias_zh: "今日开盘价" },
+    { i: 2, key: "closingPrice", alias_zh: "昨日收盘价" },
+    { i: 3, key: "presentPrice", alias_zh: "当前价格" },
+    { i: 4, key: "topPrice", alias_zh: "今日最高价" },
+    { i: 5, key: "bottomPrice", alias_zh: "今日最低价" },
+    { i: 6, key: "buyingPrice1", alias_zh: "买一" },
+    { i: 7, key: "sellingPrice1", alias_zh: "卖一" },
+    { i: 8, key: "turnoverStocks", alias_zh: "成交量" },
+    { i: 9, key: "turnoverPrice", alias_zh: "成交额" },
+    { i: 30, key: "date", alias_zh: "日期" },
+    { i: 31, key: "time", alias_zh: "时间" }
+];
+
 const state = {
-  stockStation: {
-    stockCodes: ["sh601816"]
-  }
+    stockStation: {
+        stockCodes: ["sh601816", "sh000001"],
+        stockStore:[]
+    }
 };
 
-const getters = {};
+const getters = {
+    translateStocksData: () => data => {
+        // split Item
+
+        var stocksList = data.split(";");
+        var res = {};
+        stocksList.pop();
+        stocksList.forEach(item => {
+            var item_key_data = item.split("=");
+            var key = item_key_data[0].split("_").pop();
+            var data = item_key_data[1].replace(/"/g, "").split(",");
+            var dataObj = {};
+            data.forEach((dataItem, index) => {
+                var itemFormat = formater.find(format => format.i === index);
+                itemFormat && (dataObj[itemFormat.key] = dataItem);
+            });
+            dataObj[formater[0].key] = key;
+            res[key] = dataObj;
+        });
+
+        return res;
+    }
+};
 
 var actions = {
-  getStocks({ state }) {
-    Api.getStockByCodes(state.stockStation.stockCodes).then(res => {
-      console.log(res);
-    });
-  },
-
-  test() {
-    console.log("hello");
-  }
+    getStocks({ state, getters, commit }) {
+        Api.getStockByCodes(state.stockStation.stockCodes)
+            .then(res => {
+                console.log(res);
+                if (res.status === 200) {
+                    var transData = getters.translateStocksData(res.data);
+                    this.commit('stock/changeStockStore',transData);
+                } else {
+                    throw new Error(res.statusText);
+                }
+            })
+            .catch(err => {
+                console.warn(err);
+            });
+    }
 };
 
 const mutations = {
-  test() {
-    console.log("test");
-  }
+    changeStockStore(state,data){
+      var stockStore = state.stockStation.stockStore;
+      Object.keys(data).forEach(key=>{
+        var stockInfo = data[key];
+        !stockStore[key] && (stockStore[key] = []);
+        stockStore[key].push(stockInfo);
+      });
+    }
 };
 
 export default {
-  namespaced: true,
-  state,
-  getters,
-  actions,
-  mutations
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
 };
